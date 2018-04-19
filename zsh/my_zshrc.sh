3f098fd948044415edfecb4379590d0dc9525470
komoriyuichiro <thekomori1113@gmail.com>
2018-04-20 05:29:28 +0900

    fix RPROMPT

--- zsh/my_zshrc.sh
+++ zsh/my_zshrc.sh
@@ -50,11 +50,55 @@ alias lla='ls -la'
 alias l1='ls -1'
 alias la1='ls -1a'
 
+## RPROMPT
+RPROMPT=$'`branch-status-check`' # %~はpwd
+setopt prompt_subst
+
+# {{{ methods for RPROMPT
+# fg[color]表記と$reset_colorを使いたい
+# @see https://wiki.archlinux.org/index.php/zsh
+autoload -U colors; colors
+function branch-status-check {
+    local prefix branchname suffix
+        # .gitの中だから除外
+        if [[ "$PWD" =~ '/\.git(/.*)?$' ]]; then
+            return
+        fi
+        branchname=`get-branch-name`
+        # ブランチ名が無いので除外
+        if [[ -z $branchname ]]; then
+            return
+        fi
+        prefix=`get-branch-status` #色だけ返ってくる
+        suffix='%{'${reset_color}'%}'
+        echo ${prefix}'['${branchname}']'${suffix}
+}
+function get-branch-name {
+    # gitディレクトリじゃない場合のエラーは捨てます
+    echo `git rev-parse --abbrev-ref HEAD 2> /dev/null`
+}
+function get-branch-status {
+    local res color
+        output=`git status --short 2> /dev/null`
+        if [ -z "$output" ]; then
+            res=':' # status Clean
+            color='%{'${fg[green]}'%}'
+        elif [[ $output =~ "[\n]?\?\? " ]]; then
+            res='?:' # Untracked
+            color='%{'${fg[yellow]}'%}'
+        elif [[ $output =~ "[\n]? M " ]]; then
+            res='M:' # Modified
+            color='%{'${fg[red]}'%}'
+        else
+            res='A:' # Added to commit
+            color='%{'${fg[cyan]}'%}'
+        fi
+        # echo ${color}${res}'%{'${reset_color}'%}'
+        echo ${color} # 色だけ返す
+}
+
 PROMPT="%{${fg[cyan]}%}[%T]%{${reset_color}%} %~
 %# "
-
-# zstyle ':vcs_info:*' formats '%F{green}[%b]%f'
-# zstyle ':vcs_info:*' actionformats '%F{red}[%b|%a]%f'
 
 zmodload zsh/datetime
 reset_tmout() { TMOUT=$[60-EPOCHESECONDS%60] }
