de74b77d0f51c9dd5aef63960bd342314cd97ade
komoriyuichiro <thekomori1113@gmail.com>
2018-06-23 18:55:09 +0900

    add iterm2 settings file

--- scripts.sh
+++ scripts.sh
@@ -3,3 +3,5 @@
 ln -s ~/.dotfiles/zsh/my_zshrc.sh ~/.zshrc
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/hyper/.hyper.js ~/.hyper.js
+mkdir -p ~/iterm2
+ln -s ~/.dotfiles/iterm2/com.googlecode.iterm2.plist ~/iterm2/com.googlecode.iterm2.plist
