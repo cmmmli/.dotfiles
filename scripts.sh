9f24b4894aec77c52e669c1a8f680c5c2a1d1bfb
komoriyuichiro <thekomori1113@gmail.com>
2018-06-25 14:23:57 +0900

    [modify] Change reference of 'ln' command's sourcefile to Relative path from current directory

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -s ~/.dotfiles/zsh/my_zshrc.sh ~/.zshrc
-ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/hyper/.hyper.js ~/.hyper.js
+ln -s ./zsh/my_zshrc.sh ~/.zshrc
+ln -s ./vim/.vimrc ~/.vimrc
+ln -s ./hyper/.hyper.js ~/.hyper.js
 mkdir -p ~/iterm2
-ln -s ~/.dotfiles/iterm2/com.googlecode.iterm2.plist ~/iterm2/com.googlecode.iterm2.plist
+ln -s ./iterm2/com.googlecode.iterm2.plist ~/iterm2/com.googlecode.iterm2.plist
