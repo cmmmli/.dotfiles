2ca3e6808819d6b48c208fe461b4cc8e9aa580b1
komoriyuichiro <thekomori1113@gmail.com>
2018-09-25 03:08:10 +0900

    [add] git settings

--- scripts.sh
+++ scripts.sh
@@ -3,6 +3,8 @@
 ln -s ./zsh/my_zshrc.sh ~/.zshrc
 ln -s ./vim/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 
 mkdir -p ~/iterm2
 ln -s ./iterm2/com.googlecode.iterm2.plist ~/iterm2/com.googlecode.iterm2.plist
